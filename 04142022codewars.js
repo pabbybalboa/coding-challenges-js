6c20a946ec999a04b47c3a8879d0406cc617b622
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-04-14 20:56:53 -0700

    CodeWars 04/14/2022 String into array of strings

---
+++ 04142022codewars.js
@@ -0,0 +1,11 @@
+//CODEWARS
+//04-14-2022
+
+//Write a function that splits a string and converts it into an array of words.
+
+function stringToArray(string){
+    string = string.split('')
+    return(string)
+}
+
+//The split method turns a string into an array of substrings. by using the " " we state that what seperates the strings in the array is the space.
