0be12b985fe5bf8413412ca6fa619c68d1ac6cdf
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-04-16 11:57:42 -0700

    04162022 CodeWars string to number

---
+++ 04162022codewars.js
@@ -0,0 +1,10 @@
+//CODEWARS
+//04-16-2022
+
+//We need a function that can convert a string to a number. 
+//For this we can use the Number() method
+
+let stringToNumber = function(str){
+    str = Number(str)
+    return str
+}
