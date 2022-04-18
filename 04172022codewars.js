429bf014c85fce818d055c6adb2b2afabb1cd470
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-04-17 23:02:06 -0700

    CodeWars 04/17/2022 multiplying in if situations

---
+++ 04172022codewars.js
@@ -0,0 +1,14 @@
+//CODEWARS
+//04-17-2022
+
+//Make a function that multiplies a number by 8 if it is even and multiplies a number by 9 if it is odd.
+
+function simpleMultiply(number){
+    if(number % 2 === 0){
+        return number * 8
+    }else{
+        return number * 9
+    }
+}
+
+//Here remember to use three = signs as they are for equation not for an assignment.
