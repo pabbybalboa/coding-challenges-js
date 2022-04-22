8bbc41c9f53c40ac334eeddd525402b928956b25
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-04-22 00:04:06 -0700

    Codewars 04222022 Even or odd conditional

---
+++ 04222022codewars.js
@@ -0,0 +1,11 @@
+//CODEWARS
+//04-22-2022
+//Create a function that if a number is even return "Even" and if odd returns "Odd"
+
+function evenOrOdd(number){
+    if(number%2==0){
+        return "Even"
+    }else{
+        return "Odd"
+    }
+}
