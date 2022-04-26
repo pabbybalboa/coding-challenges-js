673859fdb460751e12fdd77d3c5adbc1bb884001
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-04-25 21:22:37 -0700

    CodeWars 04/25/2022 Reverse interger.

---
+++ 04252022codewars.js
@@ -0,0 +1,10 @@
+//CODEWARS
+//04-25-2022
+
+//Given a set of numbers, find its opposite and return it.
+
+function opposite(number){
+    return number * -1
+}
+
+//Inverts the number. If -5, -5 * -1 = 5. If 5, 5 * -1 = -5.
