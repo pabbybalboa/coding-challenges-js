3fd935c5fc7400c44bd433041463402f3cb7ab42
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-06-01 23:43:34 -0700

    06012022 code wars conditional

---
+++ 06012022codewars.js
@@ -0,0 +1,15 @@
+//CODEWARS
+//06-01-2022
+
+//Find if the base is divisible by the factor.
+//Return true if it is, false if it's not.
+
+function checkForFactor (base, factor) {
+    if(base % factor == 0){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  //simple conditional
