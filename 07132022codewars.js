cf245462458cbe796ac8f3a143f50286aa658290
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-07-13 22:24:52 -0700

    CodeWars 07-13-2022 array of strings into a new string

---
+++ 07132022codewars.js
@@ -0,0 +1,10 @@
+//CodeWars
+//July 13th, 2022
+
+//turn an array of strings into a new string that is a sentence of those strings
+
+function smash (words){
+    return words.join(" ")
+}
+
+//the operator join, joins an array together and the seperator is what seperates the strings in the new string sentence
